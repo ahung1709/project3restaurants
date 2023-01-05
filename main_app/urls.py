406bed192cdcd4ec64f5c352f256585caa9486d1
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2023-01-05 18:12:41 -0500

    clean code

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,51 +7,28 @@ urlpatterns = [
     path('about/', views.about, name='about'),
 
     ### Restaurant routes ###
-
-    path('all_restaurants/', views.all_restaurants_index,
-         name='all_restaurants_index'),
-    path('all_restaurants/<int:restaurant_id>/',
-         views.all_restaurants_detail, name='all_restaurants_detail'),
+    path('all_restaurants/', views.all_restaurants_index, name='all_restaurants_index'),
+    path('all_restaurants/<int:restaurant_id>/', views.all_restaurants_detail, name='all_restaurants_detail'),
 
     path('restaurants/', views.restaurants_index, name='restaurants_index'),
-    path('restaurants/<int:pk>/', views.RestaurantDetail.as_view(),
-         name='restaurants_detail'),
+    path('restaurants/<int:pk>/', views.RestaurantDetail.as_view(), name='restaurants_detail'),
     path('restaurants/create/', views.restaurant_create, name='restaurants_create'),
-    path('restaurants/<int:restaurant_id>/update/',
-         views.restaurant_update, name='restaurants_update'),
-    path('restaurants/<int:pk>/delete/',
-         views.RestaurantDelete.as_view(), name='restaurants_delete'),
-    path('restaurants/<int:restaurant_id>/add_review/',
-         views.add_review, name='add_review'),
-    path('restaurants/<int:restaurant_id>/delete_review/<int:review_id>/',
-         views.delete_review, name='delete_review'),
+    path('restaurants/<int:restaurant_id>/update/', views.restaurant_update, name='restaurants_update'),
+    path('restaurants/<int:pk>/delete/', views.RestaurantDelete.as_view(), name='restaurants_delete'),
+    path('restaurants/<int:restaurant_id>/add_review/', views.add_review, name='add_review'),
+    path('restaurants/<int:restaurant_id>/delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
 
     ### Account routes ###
     path('accounts/signup/', views.signup, name='signup'),
-    # ok
     path('accounts/profile/', views.user_profile, name='user_profile'),
     path('accounts/update/', views.user_update, name='user_update'),
-    path('accounts/delete/', views.user_delete_confirm,
-         name='user_delete_confirm'),
+    path('accounts/delete/', views.user_delete_confirm, name='user_delete_confirm'),
 
-    path('accounts/password_change/',
-         views.ChangePasswordView.as_view(), name='password_change'),
-    path('accounts/password_change_done/',
-         views.pwd_change_done, name='pwd_change_done'),
+    path('accounts/password_change/', views.ChangePasswordView.as_view(), name='password_change'),
+    path('accounts/password_change_done/', views.pwd_change_done, name='pwd_change_done'),
 
-    # favorites
+    ### favorites ###
     path('favorites/', views.list_favorites, name='favorites'),
-    #     path('restaurants/<int:restaurant_id>/favorites/create/',
-    #          views.CreateView.as_view(), name='create-favourite'),
-
-    #     path('favorites/<int:pk>/', views.favorite_detail, name='favorite-details'),
-    #     path('restaurants/<int:pk>/favorites/delete/<int:favorite_pk>/',
-    #          views.FavoriteDelete.as_view(),  name='favorite-delete'),
-    #     path('restaurants/<int:pk>/favorites/update/<int:favorite_pk>/',
-    #          views.UpdateFavorite.as_view(), name='favorite-update'),
-
-    path('restaurants/<int:restaurant_id>/favorites/',
-         views.assoc_fav, name='associate'),
-    path('restaurants/<int:restaurant_id>/unfavorites/',
-         views.unassoc_fav, name='unassociate')
+    path('restaurants/<int:restaurant_id>/favorites/', views.assoc_fav, name='associate'),
+    path('restaurants/<int:restaurant_id>/unfavorites/', views.unassoc_fav, name='unassociate')
 ]
